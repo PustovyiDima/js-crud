3b99d0ef3f43690a96695302ca5c0e44e82cd8d4
pustovyidima <pustovoydima@gmail.com>
2023-08-08 18:48:06 +0300

    done product crud

--- src/route/index.js
+++ src/route/index.js
@@ -55,14 +55,111 @@ class User {
     }
   }
 }
-// ================================================================
 
+class Product {
+  static #list = [
+    {
+      id: 10001,
+      name: 'Product 1',
+      price: 100,
+      description: 'some text',
+      createDate: new Date(),
+    },
+    {
+      id: 10002,
+      name: 'Product 2',
+      price: 200,
+      description:
+        'some text some textsome textsome textsome textsome textsome textsome textsome text',
+      createDate: new Date(),
+    },
+    {
+      id: 10003,
+      name: 'Product 3',
+      price: 300,
+      description: 'some text',
+      createDate: new Date(),
+    },
+  ]
+  id = Product.getId()
+
+  constructor(name, price, description) {
+    this.name = name
+    this.price = price
+    this.description = description
+    this.createDate = new Date()
+  }
+
+  static getId = () => {
+    let res = ''
+    for (let i = 0; i < 5; i++) {
+      res += Math.round(Math.random() * 9)
+    }
+    return Number(res)
+  }
+
+  static getList = () => {
+    return this.#list
+  }
+
+  static addProduct = (product) => {
+    this.#list.push(product)
+  }
+
+  static getById = (id) => {
+    return this.#list.find((product) => product.id === id)
+  }
+
+  static deleteById = (id) => {
+    const index = this.#list.findIndex(
+      (product) => product.id === id,
+    )
+    if (index !== -1) {
+      this.#list.splice(index, 1)
+      return true
+    } else {
+      return false
+    }
+  }
+
+  static updateById = (id, data) => {
+    const product = Product.getById(Number(id))
+
+    if (product) {
+      Product.update(product, data)
+      return true
+    } else {
+      return false
+    }
+  }
+  static update = (
+    product,
+    { name, price, description },
+  ) => {
+    if (name) {
+      product.name = name
+    }
+    if (price) {
+      product.price = price
+    }
+    if (description) {
+      product.description = description
+    }
+  }
+}
+// ================================================================
 router.get('/', function (req, res) {
+  res.render('index', {
+    style: 'index',
+  })
+})
+// ========================USER====================================
+router.get('/user', function (req, res) {
   const list = User.getList()
 
-  res.render('index', {
+  res.render('user', {
     // вказуємо назву папки контейнера, в якій знаходяться наші стилі
-    style: 'index',
+    style: 'user',
     data: {
       users: {
         list,
@@ -70,7 +167,6 @@ router.get('/', function (req, res) {
       },
     },
   })
-  // ↑↑ сюди вводимо JSON дані
 })
 
 // ================================================================
@@ -121,7 +217,109 @@ router.post('/user-update', function (req, res) {
       : `Помилка обробки данних`,
   })
 })
+// ========================PRODUCT=================================
+router.get('/product', function (req, res) {
+  res.render('product', {
+    style: 'product',
+  })
+})
 
+router.post('/product-created', function (req, res) {
+  const { name, price, description } = req.body
+
+  const product = new Product(name, price, description)
+
+  Product.addProduct(product)
+
+  // console.log(Product.getList())
+
+  res.render('product-alert', {
+    style: 'product-alert',
+    result: `Успішне виконання дії`,
+    text: 'Товар успішно доданий',
+  })
+})
+
+router.get('/product-create', function (req, res) {
+  res.render('product-create', {
+    style: 'product-create',
+  })
+})
+
+router.get('/product-list', function (req, res) {
+  const list = Product.getList()
+  res.render('product-list', {
+    style: 'product-list',
+    data: {
+      products: {
+        list,
+        isEmpty: list.length === 0,
+      },
+    },
+  })
+})
+
+router.get('/product-edit', function (req, res) {
+  const { id } = req.query
+  const product = Product.getById(Number(id))
+  console.log(product)
+  res.render('product-edit', {
+    style: 'product-edit',
+    product,
+  })
+})
+router.post('/product-edited', function (req, res) {
+  const { name, price, id, description } = req.body
+
+  let res1 = false
+  let res2 = false
+  if (
+    Product.updateById(id, {
+      name,
+      price,
+      description,
+    })
+  ) {
+    res2 = true
+    res1 = true
+  }
+  res.render('product-alert', {
+    style: 'product-alert',
+
+    result: res1
+      ? `Успішне виконання дії`
+      : `Помилка обробки`,
+    text: res2
+      ? 'Товар успішно оновлений'
+      : 'Товар відсутній в системі',
+  })
+})
+
+router.post('/product-delete', function (req, res) {
+  const { id } = req.body
+  let res1 = false
+  let res2 = false
+  if (Product.deleteById(Number(id))) {
+    res2 = true
+    res1 = true
+  }
+  res.render('product-alert', {
+    style: 'product-alert',
+
+    result: res1
+      ? `Успішне виконання дії`
+      : `Помилка обробки`,
+    text: res2
+      ? 'Товар успішно видалений'
+      : 'Товар відсутній в системі',
+  })
+})
+
+router.get('/product-alert', function (req, res) {
+  res.render('product-alert', {
+    style: 'product-alert',
+  })
+})
 // ================================================================
 
 // Підключаємо роутер до бек-енду
